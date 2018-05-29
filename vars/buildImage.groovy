9e374f3263021611658b4553c3c84bbee57ff856
Alexander Katsenelenbogen <alex@Alexanders-MacBook-Pro.local>
2018-05-29 18:38:09 -0400

    DEVOPS-34: try the pipeline library

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,15 @@
-def call() {
+def call(imageName, releaseNumber) {
   echo "Hello!";
-  sh "ls"
+  sh """
+            export AWS_DEFAULT_REGION="us-east-1"
+            echo "Logging in to Docker via AWS"
+            _DOCKER_REPO="\$(aws ecr get-authorization-token --output text  --query 'authorizationData[].proxyEndpoint')"
+            aws ecr get-login --no-include-email --region us-east-1 | awk '{print \$6}' | docker login -u AWS --password-stdin \$_DOCKER_REPO
+            echo "Building docker image"
+            cd devops-ui
+            docker build -t "${imageName}:${releaseNumber}" .
+            _DOCKER_REPO_NO_PRE="\$(echo \$_DOCKER_REPO | sed 's~http[s]*://~~g')"
+            docker tag "${imageName}:${releaseNumber}" "\$_DOCKER_REPO_NO_PRE/${imageName}:${releaseNumber}"
+            docker push "\$_DOCKER_REPO_NO_PRE/${imageName}:${releaseNumber}"
+     """
 }
