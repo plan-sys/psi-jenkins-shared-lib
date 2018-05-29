cfaffe8b4381a084e0972a837ae6af9523875d92
Alexander Katsenelenbogen <alex@Alexanders-MacBook-Pro.local>
2018-05-29 19:02:24 -0400

    DEVOPS-34: account for build arguments

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,4 @@
-def call(imageName, releaseNumber, projectFolder) {
+def call(imageName, releaseNumber, projectFolder, buildArgString) {
   echo "Hello!";
   sh """
             export AWS_DEFAULT_REGION="us-east-1"
@@ -7,7 +7,7 @@ def call(imageName, releaseNumber, projectFolder) {
             aws ecr get-login --no-include-email --region us-east-1 | awk '{print \$6}' | docker login -u AWS --password-stdin \$_DOCKER_REPO
             echo "Building docker image"
             cd ${projectFolder}
-            docker build -t "${imageName}:${releaseNumber}" .
+            docker build -t "${imageName}:${releaseNumber}" ${buildArgString} .
             _DOCKER_REPO_NO_PRE="\$(echo \$_DOCKER_REPO | sed 's~http[s]*://~~g')"
             docker tag "${imageName}:${releaseNumber}" "\$_DOCKER_REPO_NO_PRE/${imageName}:${releaseNumber}"
             docker push "\$_DOCKER_REPO_NO_PRE/${imageName}:${releaseNumber}"
