86dbe0cafc09f44c708c78a80fc6ee72a5ca2203
Lenard Williams <lenard.dev@gmail.com>
2018-07-20 21:35:58 -0400

    DEVOPS-43: Adding additional parameters for task memory and cpu

--- vars/updateAWSTask.groovy
+++ vars/updateAWSTask.groovy
@@ -1,4 +1,4 @@
-def call(taskFamily, cluster, service, dockerImageName, releaseNumber){
+def call(taskFamily, cluster, service, dockerImageName, releaseNumber, memory, cpu){
   sh """
       export AWS_DEFAULT_REGION="us-east-1"
       TASK_FAMILY=${taskFamily}
@@ -6,12 +6,14 @@ def call(taskFamily, cluster, service, dockerImageName, releaseNumber){
       NEW_DOCKER_IMAGE="341373978347.dkr.ecr.us-east-1.amazonaws.com/${dockerImageName}:${releaseNumber}"
       CLUSTER_NAME=${cluster}
       RELEASE_NUMBER=${releaseNumber}
+      TASK_MEMORY=${releaseNumber}
+      TASK_CPU=${releaseNumber}
 
       OLD_TASK_DEF=\$(aws ecs describe-task-definition --task-definition \$TASK_FAMILY --output json)
       TASK_WITH_NEW_VERSION_NUM=\$(echo \$OLD_TASK_DEF | jq -r --arg RELEASE_NUMBER \$RELEASE_NUMBER '.taskDefinition.containerDefinitions[0].environment |= map(if .name=="RELEASE_VERSION" then .value=\$RELEASE_NUMBER else . end)')
       NEW_TASK_DEF=\$(echo \$TASK_WITH_NEW_VERSION_NUM | jq --arg NDI \$NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=\$NDI')
       FINAL_TASK=\$(echo \$NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
-      aws ecs register-task-definition --family \$TASK_FAMILY --network-mode "awsvpc" --memory 512 --cpu 256 --execution-role-arn "arn:aws:iam::341373978347:role/ecsTaskExecutionRole" --task-role-arn "arn:aws:iam::341373978347:role/ecsTaskExecutionRole" --requires-compatibilities "FARGATE" --cli-input-json "\$(echo \$FINAL_TASK)"
+      aws ecs register-task-definition --family \$TASK_FAMILY --network-mode "awsvpc" --memory $TASK_MEMORY --cpu $TASK_CPU --execution-role-arn "arn:aws:iam::341373978347:role/ecsTaskExecutionRole" --task-role-arn "arn:aws:iam::341373978347:role/ecsTaskExecutionRole" --requires-compatibilities "FARGATE" --cli-input-json "\$(echo \$FINAL_TASK)"
       aws ecs update-service --service \$SERVICE_NAME --task-definition \$TASK_FAMILY --cluster \$CLUSTER_NAME
       """
 }
