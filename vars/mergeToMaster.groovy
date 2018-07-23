0cb14ec9b49f388055a8f55a78b8389a5ac49448
Lenard Williams <36276904+lenardw1@users.noreply.github.com>
2018-07-22 23:13:30 -0400

    DEVOPS-62: Adding merging branch parameter

--- vars/mergeToMaster.groovy
+++ vars/mergeToMaster.groovy
@@ -1,11 +1,11 @@
-def call() {
+def call(mergingBranch) {
     
     sh """
         echo "Merging to Master"
         git remote set-head origin master
         git checkout -B master origin/master
         git pull origin master
-        git pull origin staging
+        git pull origin ${mergingBranch}
         git push --set-upstream origin master
        """
 }
